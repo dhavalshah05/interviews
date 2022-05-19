36934c1be03c1d32e3fcf096090548b8de2de1cf
Dhaval Shah <dhaval@alphastack.in>
2022-05-19 11:42:32 +0530

    Updated settings screen

--- app/src/main/java/com/template/app/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/template/app/ui/settings/SettingsFragment.kt
@@ -4,8 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.ViewCompositionStrategy
 import androidx.fragment.app.Fragment
-import com.template.app.databinding.SettingsFragmentBinding
+import com.template.app.R
 import com.template.app.ui.common.navigator.AppNavigator
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
@@ -16,41 +19,42 @@ class SettingsFragment : Fragment() {
     @Inject
     lateinit var navigator: AppNavigator
 
-    private var _binding: SettingsFragmentBinding? = null
-    private val binding get() = _binding!!
+    private val settingOptions = mutableStateOf(
+        listOf(
+            SettingOption(id = 1, title = R.string.label_hr_managers),
+            SettingOption(id = 2, title = R.string.label_interviewers),
+        )
+    )
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return SettingsFragmentBinding.inflate(inflater, container, false).root
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        _binding = SettingsFragmentBinding.bind(view)
-        initToolbar()
-        initViewListeners()
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
-    }
-
-    private fun initToolbar() {
-        binding.toolbar.setNavigationOnClickListener {
-            navigator.goBack()
+        return ComposeView(requireContext()).apply {
+            setViewCompositionStrategy(ViewCompositionStrategy.Default)
+            setContent {
+                SettingsScreen(
+                    settingOptions = settingOptions.value,
+                    onBackClick = onBackClick,
+                    onSettingOptionClick = onSettingOptionClick
+                )
+            }
         }
     }
 
-    private fun initViewListeners() {
-        binding.textViewManagers.setOnClickListener {
+    private val onBackClick: () -> Unit = {
+        navigator.goBack()
+    }
+
+    private val onSettingOptionClick: (SettingOption) -> Unit = { settingOption ->
+        if (settingOption.title == R.string.label_hr_managers) {
             navigator.openManagersScreen()
         }
-        binding.textViewInterviewers.setOnClickListener {
+
+        if (settingOption.title == R.string.label_interviewers) {
             navigator.openInterviewersScreen()
         }
     }
+
 }
