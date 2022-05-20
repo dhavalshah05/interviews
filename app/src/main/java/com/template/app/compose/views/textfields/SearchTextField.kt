27a1ead81cadf0994576a00a0bac44dffc1cbd4a
Dhaval Shah <dhaval@alphastack.in>
2022-05-20 13:53:36 +0530

    Extracted AppTextField

--- app/src/main/java/com/template/app/compose/views/textfields/SearchTextField.kt
+++ app/src/main/java/com/template/app/compose/views/textfields/SearchTextField.kt
@@ -1,8 +1,6 @@
 package com.template.app.compose.views.textfields
 
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
@@ -12,16 +10,13 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalFocusManager
-import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.template.app.R
-import com.template.app.compose.views.texts.AppTextStyle
 
 @Preview
 @Composable
@@ -45,28 +40,11 @@ fun SearchTextField(
     onClearClick: () -> Unit,
     onDoneClick: () -> Unit
 ) {
-    TextField(
+    AppTextField(
+        placeHolderText = stringResource(id = R.string.hint_search),
+        modifier = modifier,
         value = value,
         onValueChange = onValueChange,
-        modifier = modifier
-            .fillMaxWidth()
-            .border(width = 1.dp, color = colorResource(id = R.color.cardBorder), shape = RoundedCornerShape(4.dp)),
-        singleLine = true,
-        keyboardOptions = KeyboardOptions(
-            keyboardType = KeyboardType.Text,
-            imeAction = ImeAction.Done
-        ),
-        keyboardActions = KeyboardActions(
-            onDone = { onDoneClick.invoke() }
-        ),
-        placeholder = {
-            Text(
-                text = stringResource(id = R.string.hint_search),
-                style = AppTextStyle.Regular,
-                fontSize = 14.sp,
-                color = colorResource(id = R.color.textSecondary)
-            )
-        },
         leadingIcon = {
             Icon(painter = painterResource(id = R.drawable.ic_search), contentDescription = "Search Icon", tint = Color.Unspecified)
         },
@@ -79,19 +57,12 @@ fun SearchTextField(
                 }
             }
         },
-        shape = RoundedCornerShape(4.dp),
-        colors = TextFieldDefaults.textFieldColors(
-            errorIndicatorColor = Color.Transparent,
-            focusedIndicatorColor = Color.Transparent,
-            unfocusedIndicatorColor = Color.Transparent,
-            disabledIndicatorColor = Color.Transparent,
-            cursorColor = colorResource(id = R.color.primary),
-            errorCursorColor = colorResource(id = R.color.primary),
-            textColor = colorResource(id = R.color.primary),
-            backgroundColor = colorResource(id = R.color.cardBackground)
+        keyboardActions = KeyboardActions(
+            onDone = { onDoneClick.invoke() }
         ),
-        textStyle = AppTextStyle.Regular.copy(
-            fontSize = 14.sp
-        ),
+        keyboardOptions = KeyboardOptions(
+            keyboardType = KeyboardType.Text,
+            imeAction = ImeAction.Done
+        )
     )
 }
